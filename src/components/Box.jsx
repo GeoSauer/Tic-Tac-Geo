7e370ff5b284d5dd0a16ab8ad32203d8fda9f281
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-27 10:46:43 -0700

    add clickHandler with console to prove we clickin

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,5 +1,16 @@
+import { useGameContext } from '../context/GameContext';
 import './Box.css';
 
 export default function Box({ box }) {
-  return <div className="btn">{box.content}</div>;
+  const { currentPlayer, setCurrentPlayer, board, setBoard, isActive } = useGameContext();
+
+  function clickHandler() {
+    console.log('wow what a click!');
+  }
+
+  return (
+    <div className="btn" onClick={clickHandler}>
+      {box.content}
+    </div>
+  );
 }
